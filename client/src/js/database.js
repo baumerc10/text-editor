938dba32991cefe17f742b03cc6286a3624805d3
Cody Kirschbaum <baumerc10@gmail.com>
2023-02-09 18:49:53 -0500

    added files not in source code + get/put requests

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('Request to update database.');
+  const contentDb = await openDB('jate', 1);
+  const trans = contentDb.transaction('jate', 'readwrite');
+  const store = trans.objectStore('jate');
+  const req = store.put({id: id, value: content});
+  const res = await req;
+  console.log('Data updated and saved to the database.', res);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (content) => {
+  console.log('Getting data.');
+  const contentDb = await openDB('jate', 1);
+  const trans = contentDb.transaction('jate', 'readwrite');
+  const store = trans.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+  console.log('Data saved to the database.', res);
+}
 
 initdb();
